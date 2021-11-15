2ba4cae0c866ce14e9fd98020d051bee825ce35e
Justin <justinlamarr1992@icloud.com>
2021-11-14 22:49:34 -0600

    Npm installs up to Section 7

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { readdirSync } from "fs";
+import cors from "cors";
+import mongoose from "mongoose";
+const morgan = require("morgan");
+require("dotenv").config();
+
+const app = express();
+
+// db connection
+mongoose
+  .connect(process.env.DATABASEHOME, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB Connection Error: ", err));
+
+// middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+// route middleware
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Service is running on port ${port}`));
